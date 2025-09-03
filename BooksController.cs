5c863644f88b85fefcc154a228fa7ef3a0ad0ef6
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-09-02 20:11:22 -0400

    Add Post, Put and Delete with ActionResults

--- BooksController.cs
+++ BooksController.cs
@@ -1,6 +1,8 @@
+using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BookApiHttpClient
 {
@@ -18,7 +20,7 @@ namespace BookApiHttpClient
         public IEnumerable<Book>? Books { get; set; }
 
         [HttpGet]
-        public async Task<IEnumerable<Book>> GetBooksAsync()
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooksAsync()
         {
             var httpClient = _httpClientFactory.CreateClient("BookFakeApi");
 
@@ -31,12 +33,11 @@ namespace BookApiHttpClient
 
                 Books = await JsonSerializer.DeserializeAsync<IEnumerable<Book>>(contentStream, new JsonSerializerOptions { PropertyNameCaseInsensitive = true })!;
             }
-
-            return Books!;
+            return Ok(Books);
         }
 
         [HttpGet("{id}")]
-        public async Task<Book> GetBookByIdAsync(int id)
+        public async Task<ActionResult<Book>> GetBookByIdAsync(int id)
         {
             var httpClient = _httpClientFactory.CreateClient("BookFakeApi");
 
@@ -48,7 +49,54 @@ namespace BookApiHttpClient
 
             httpResponseMessage.EnsureSuccessStatusCode();
 
-            return book!;
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> CreateBookAsync(Book book)
+        {
+            var httpClient = _httpClientFactory.CreateClient("BookFakeApi");
+
+            var bookJson = new StringContent(JsonSerializer.Serialize(book),
+            Encoding.UTF8,
+            Application.Json);
+
+            using var httpResponseMessage =
+                await httpClient.PostAsync("Books", bookJson);
+
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            return Created();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBookAsync(Book book)
+        {
+            var httpClient = _httpClientFactory.CreateClient("BookFakeApi");
+
+            var bookJson = new StringContent(JsonSerializer.Serialize(book),
+            Encoding.UTF8,
+            Application.Json);
+
+            using var httpResponseMessage =
+                await httpClient.PutAsync($"Books/{book.Id}", bookJson);
+
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBookAsync(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("BookFakeApi");
+
+            using var httpResponseMessage =
+                await httpClient.DeleteAsync($"Books/{id}");
+
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            return NoContent();
         }
 
     }
